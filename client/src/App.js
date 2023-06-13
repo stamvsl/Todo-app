b9887b97ce3730932c57b6035c6f4c5e5d818562
stamvsl <stam.vsl@gmail.com>
2023-06-13 23:27:10 +0300

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import React , {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
+import Addtask from './Components/Addtask';
 
 function App() {
+  const [todolist,setTodolist] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3001/todos').then(res =>{
+      setTodolist(res.data)
+    }).catch(err => console.log(err))
+  },[])
+
+
+  const addTask = newTask =>{
+    setTodolist([...todolist,newTask])
+  }
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Addtask addTask = {addTask}/>
       
     </div>
   );
--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 const schema = new mongoose.Schema(
         {
                 todo: "String",
-            },
-            {
-                timestamps: true
+                isComplete : Boolean
             });
 export default mongoose.model("Todo", schema)
