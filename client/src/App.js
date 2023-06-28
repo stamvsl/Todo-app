00010e0b4be8d1b315594c3b6b339f9d06aa4961
stamvsl <stam.vsl@gmail.com>
2023-06-28 22:27:14 +0300

    create working

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React , {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css';
 import Addtask from './Components/Addtask';
+import Todolist from './Components/Todolist';
 
 function App() {
   const [todolist,setTodolist] = useState([])
@@ -15,9 +16,30 @@ function App() {
   const addTask = newTask =>{
     setTodolist([...todolist,newTask])
   }
+
+
+  const taskComplete = task =>{
+    const newList = [...todolist]
+    newList.forEach(item=>{
+      if(item.id===task._id){
+        item.isComplete = task.isComplete
+      }
+    })
+    setTodolist(newList)
+  }
+
+
+
+  const removeTask = task =>{
+    const newList = todolist.filter(item=> !(item._id === task._id))
+    setTodolist(newList)
+  }
+
+  
   return (
     <div className="App">
       <Addtask addTask = {addTask}/>
+      <Todolist todolist = {todolist} taskComplete = {taskComplete} removeTask = {removeTask}/>
       
     </div>
   );
---
+++ client/src/Components/Todolist.js
@@ -0,0 +1,44 @@
+import React from "react";
+import CheckIcon from '@mui/icons-material/Check';
+import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
+import axios from "axios";
+
+function Todolist(props){
+    const todolist = props.todolist.map((task, index) =>{
+        const taskComplete = task => {
+            axios.put('http://localhost:3001/todos${task._id}' , {
+                _id: task.id,
+                todo: task.todo,
+                isComplete : !task.isComplete
+            }).then(res => props.taskComplete(res.data)).catch(err =>console.log(err))
+        }
+        const removeTask = id => {
+            axios.delete('http://localhost:3001/todo${id}').then(res => props.removeTask(res.data)).catch(err => console.log(err))
+        }
+        return <li key = {index}>
+            <div>
+               <CheckIcon className={task.isComplete ? 'isComplete' : 'checkicon'}/>
+               <p className= {task.isComplete ? 'taskcomplete' : ''}>{task.todo} onClick = {() =>
+               {
+                taskComplete(task)
+               }}</p>
+            </div>
+            <div>
+                <EditIcon className="edit"/>
+                <CloseIcon className="close" onClick = {() =>{
+                    removeTask(task._id)
+                }}/>
+            </div>
+        </li>
+    })
+    return(
+        <div className="tasklist">
+           <ul>
+             {todolist}
+           </ul>
+        </div>
+    )
+}
+
+export default Todolist
