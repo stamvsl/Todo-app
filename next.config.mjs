0fee1e9900d6cfa41d9d65d45cb693a16782f682
stamvsl <stam.vsl@gmail.com>
2024-09-12 13:38:00 +0300

    https problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "x-forwarded-proto",
+            value: "http",
+          },
+        ],
+        destination: "https://your-domain.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
